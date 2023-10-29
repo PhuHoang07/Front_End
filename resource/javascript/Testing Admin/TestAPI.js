18234e462715fc4df515a828cbc4d7e2fb54bb25
duyluongg <duyluongg1910@gmail.com>
2023-10-29 16:27:00 +0700

    fix showExamSchedule

--- resource/javascript/Testing Admin/TestAPI.js
+++ resource/javascript/Testing Admin/TestAPI.js
@@ -38,7 +38,8 @@ function renderExamTime() {
                 <td><button class="remove-button" onclick="showConfirmationModal(this)">Remove</button></td>
                 <td>${examTime.publishDate}</td>
                 <td>${examTime.slot}</td>
-                <td><i class="fa-solid fa-square-caret-down btn-showExamSchedule"></i></td>
+                <td><i onclick="showTable2()" class="fa-solid fa-square-caret-down btn-showExamSchedule"></i></td>
+
               `;
 
             list.appendChild(tablerow);
